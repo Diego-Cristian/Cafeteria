f497302ffcb14f4d8fe3fc1dec711481d15f0f43
diego <diego-cristian@live.com.ar>
2024-01-19 23:16:53 -0300

    js

--- js/cafe.js
+++ js/cafe.js
@@ -1,1 +1,21 @@
 let numero 
+
+/*--> Ingresar Nombre y Apellido OK
+--> Ingresar monto del Prestamo (minimo de 10.000 y un máximo de 50.000) OK
+--> Elegir cuotas a pagar (6, 12 o 18) OK
+--> Calcular interes OK
+    -- 6 cuotas = interes de 2,6--
+    -- 12 cuotas = interes de 2,4--
+    -- 18 cuotas = interes de 2,3--
+    
+-->Validar Nombre y Apellido, Monto a retirar y cuotas.
+*/
+
+//VALIDAR NOMBRE Y APELLIDO
+function validarNombre(nombre){
+    if( (nombre == " ") || (nombre.length < 3 )){
+        return false;
+}   else {
+        return true;
+    }
+}
